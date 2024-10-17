2adb02ce39e5c0511030435952b05d3fbe450b43
GabrielPradoDev <gabriel.coelho44@etec.sp.gov.br>
2024-10-16 22:49:36 -0300

    Criando o Dominio

---
+++ DevFreela.API/Enum/ProjectsStatusEnum.cs
@@ -0,0 +1,14 @@
+﻿using DevFreela.API.Entities;
+
+namespace DevFreela.API.Enum
+{
+    public enum ProjectsStatusEnum 
+    {
+        Created = 0,
+        InProgress =1,
+        Suspended = 2,
+        Cancelled = 3,
+        Completed = 4,
+        PaymentPending = 5
+    }
+}
