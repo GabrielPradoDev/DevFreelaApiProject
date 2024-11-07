2c2500d6f9d4c5acd468509a07238b8b964d77e1
GabrielPradoDev <gabriel.coelho44@etec.sp.gov.br>
2024-11-06 21:18:14 -0300

    Adicionando o padrão Decoretor com MediatR

---
+++ DevFreela.Application/Commands/InsertProject/ValidateInsertProjectCommandBehavior.cs
@@ -0,0 +1,29 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Infrastructure.Repositories;
+using MediatR;
+
+namespace DevFreela.Application.Commands.InsertProject
+{
+    public class ValidateInsertProjectCommandBehavior :
+
+        IPipelineBehavior<InsertProjectCommand, ResultViewModel<int>>
+    {
+        private readonly DevFreelaDbContext _context;
+        public ValidateInsertProjectCommandBehavior(DevFreelaDbContext context) 
+        {
+            _context = context;
+        }
+        public async Task<ResultViewModel<int>> Handle(InsertProjectCommand request, RequestHandlerDelegate<ResultViewModel<int>> next, CancellationToken cancellationToken)
+        {
+            var clientExistis = _context.Users.Any(u => u.Id == request.IdClient);
+            var freelancerExistis = _context.Users.Any(u => u.Id == request.IdFreelancer);
+
+            if(!clientExistis ||  !freelancerExistis)
+            {
+                return ResultViewModel<int>.Error("Cliente ou Freelancer inválidos");
+            }
+
+            return await next();
+        }
+    }
+}
